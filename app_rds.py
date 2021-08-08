dbe2a944fc4c6b5bfaf73fda6277b1d189e42417
Jhih-cell <chihan0529@gmail.com>
2021-08-08 15:21:53 +0800

    delete files not need

--- app_rds.py
+++ app_rds.py
@@ -50,21 +50,6 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 def index():
     return render_template("index.html")
 
-
-@app.route("/attraction/<id>")
-def attraction(id):
-    return render_template("attraction.html")
-
-
-@app.route("/booking")
-def booking():
-    return render_template("booking.html")
-
-
-@app.route("/thankyou")
-def thankyou():
-    return render_template("thankyou.html")
-
 @app.route("/content")
 def content():
     return render_template("content.html")
